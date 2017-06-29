960b16c2dd17f404ca9ba181229cc05ac842d7af
Benjamin Röjder Delnavaz <Bocom@users.noreply.github.com>
2017-06-29 11:24:45 +0200

    Styled up the notification
    Added fade in and fade out effects, added padding.
    Fixed an issue where the tray icon would stick around after closing.
    Made it so that closing the main window or closing via the tray icon properly exits the program.

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import sys
 import pytz
 from datetime import datetime, timedelta
 from PyQt5.QtCore import Qt, QEvent, QTimer, QPoint
-from PyQt5.QtGui import QIcon, QFont, QFontDatabase
-from PyQt5.QtWidgets import QApplication, QWidget, QSystemTrayIcon, QLabel, QMenu, qApp
+from PyQt5.QtGui import QIcon, QFont, QFontDatabase, QGuiApplication
+from PyQt5.QtWidgets import QApplication, qApp, QWidget, QSystemTrayIcon, QLabel, QMenu, QHBoxLayout
 
-timezone = pytz.timezone("Asia/Tokyo")
-
+#TODO: Save these in a settings file and expose them in the settings window
 pre_window_length = 2
 window_length = 30
 
+timezone = pytz.timezone("Asia/Tokyo")
 font = None
 
 class EventWindow(object):
+    pre = None
+
     def __init__(self, start):
         self.start = datetime.strptime(start, "%H:%M")
-        self.pre = self.start - timedelta(minutes=pre_window_length)
         self.end = self.start + timedelta(minutes=window_length)
+        if pre_window_length > 0:
+            self.pre = self.start - timedelta(minutes=pre_window_length)
 
     def check(self, ct):
         s = self.start.replace(year=ct.year, month=ct.month, day=ct.day, tzinfo=timezone)
@@ -25,6 +28,9 @@ class EventWindow(object):
         return ct.time() >= s.time() and ct.time() < e.time()
 
     def check_pre(self, ct):
+        if self.pre is None:
+            return False
+
         p = self.pre.replace(year=ct.year, month=ct.month, day=ct.day, tzinfo=timezone)
         s = self.start.replace(year=ct.year, month=ct.month, day=ct.day, tzinfo=timezone)
 
@@ -39,6 +45,17 @@ windows = [
 ]
 
 class Notification(QWidget):
+    target_seconds = 0.5 # TODO: Expose this in settings window
+
+    framerate = 60
+    
+    transition_complete = True
+    visible = False
+    
+    frame = 0
+    target_frames = 0
+    transition_timer = None
+
     def __init__(self, parent=None):
         super().__init__(parent)
 
@@ -46,27 +63,100 @@ class Notification(QWidget):
 
     def init(self):
         self.setWindowFlags(Qt.FramelessWindowHint|Qt.WindowStaysOnTopHint)
+        self.setAttribute(Qt.WA_ShowWithoutActivating)
         self.setWindowTitle("SINoALICE Event Window Status")
         self.setWindowIcon(QIcon("icon.png"))
-        self.resize(300, 40)
+        self.resize(300, 54)
 
         self.setStyleSheet("background-color: #3b3325;")
 
-        self.label = QLabel("Default notification text", self, font=font)
+        layout = QHBoxLayout()
+
+        self.label = QLabel("Default notification text", font=font)
         self.label.setStyleSheet("color: #ffffff;")
-        
-        sg = QApplication.desktop().availableGeometry().bottomRight()
-        p = QPoint(sg.x() - self.rect().width(), sg.y() - self.rect().height())
+
+        layout.addWidget(self.label)
+
+        self.setLayout(layout)
+
+        # TODO: Let the user to pick which screen to display the notification on
+        self.framerate = QGuiApplication.primaryScreen().refreshRate()
+        self.target_frames = self.framerate * self.target_seconds
+
+        # TODO: Let the user pick the corner that the notification shows up in
+        desktop = QApplication.desktop()
+        sg = desktop.availableGeometry().bottomRight()
+        p = QPoint(sg.x() - self.width() - 1, sg.y() - self.height())
 
         self.move(p)
 
+    def start_show_transition(self):
+        self.fade_in = True
+
+        self.setWindowOpacity(0)
+        self.show()
+
+        self._start_transition()
+
+    def start_hide_transition(self):
+        self.fade_in = False
+
+        self.setWindowOpacity(1)
+
+        self._start_transition()
+
+    def _start_transition(self):
+        self.transition_complete = False
+
+        self.frame = 0
+
+        self.transition_timer = QTimer(self)
+        self.transition_timer.setSingleShot(False)
+        self.transition_timer.timeout.connect(self.update_transition)
+        self.transition_timer.start((1 / self.framerate) * 1000)
+
+    def update_transition(self):
+        self.frame += 1
+        
+        if self.fade_in:
+            opacity = self.frame / self.target_frames
+        else:
+            opacity = (self.target_frames - self.frame) / self.target_frames
+
+        self.setWindowOpacity(opacity)
+
+        if self.frame == self.target_frames:
+            self.transition_timer.stop()
+            self.transition_complete = True
+
+            if self.fade_in:
+                self.visible = True
+            else:
+                self.visible = False
+                super().hide()
+
     def show_regular(self):
         self.label.setText("Upgrade Fodder window open!")
-        self.show()
+
+        # TODO: Play sound?
+
+        if self.visible:
+            return
+
+        self.start_show_transition()
 
     def show_pre(self):
         self.label.setText("A window is about to open!")
-        self.show()
+
+        #TODO: Play sound?
+
+        if self.visible:
+            return
+        
+        self.start_show_transition()
+
+    def hide(self):
+        self.start_hide_transition()
 
 class Window(QWidget):
     notification = None
@@ -77,11 +167,18 @@ class Window(QWidget):
 
         self.init()
 
+        self.notification = Notification()
+
+        self.tray_icon = TrayIcon(QIcon("icon.png"), self)
+        self.tray_icon.show()
+
+        self.update()
+
     def init(self):
         self.setWindowTitle("SINoALICE Upgrade Fodder Event Alerter")
         self.setWindowIcon(QIcon("icon.png"))
 
-        self.resize(250, 150)
+        self.resize(300, 75)
 
         self.temp_label = QLabel("There'll be options here later", self, font=font)
         
@@ -97,8 +194,6 @@ class Window(QWidget):
         self.timer.start(1000 * 30)
 
         self.show()
-
-        self.update()
 
     def update(self):
         ct = datetime.now(timezone)
@@ -116,30 +211,15 @@ class Window(QWidget):
                 break
 
         if in_pre_window:
-            self.showNotification(kind="pre")
+            self.notification.show_pre()
         elif in_window:
-            self.showNotification(kind="regular")
+            self.notification.show_regular()
         else:
-            self.hideNotification()
+            self.notification.hide()
 
     def unhide(self):
         self.setWindowState(Qt.WindowActive)
         QTimer.singleShot(0, self.show)
-
-    def showNotification(self, kind="regular"):
-        if self.notification is None:
-            self.notification = Notification()
-
-        if kind == "regular":
-            self.notification.show_regular()
-        elif kind == "pre":
-            self.notification.show_pre()
-
-    def hideNotification(self):
-        if self.notification is None:
-            return
-
-        self.notification.hide()
 
     def changeEvent(self, event):
         if event.type() == QEvent.WindowStateChange:
@@ -148,8 +228,16 @@ class Window(QWidget):
 
         super().changeEvent(event)
 
+    def closeEvent(self, event):
+        self.tray_icon.hide()
+
+        if self.notification.isVisible():
+            self.notification.close()
+
+        event.accept()
+
 class TrayIcon(QSystemTrayIcon):
-    def __init__(self, icon, parent = None):
+    def __init__(self, icon, parent=None):
         super().__init__(icon, parent)
 
         self.setToolTip("SINoALICE Upgrade Fodder Event Alerter")
@@ -162,9 +250,15 @@ class TrayIcon(QSystemTrayIcon):
         menu.addSeparator()
 
         exit_action = menu.addAction("Exit")
-        exit_action.triggered.connect(qApp.quit)
+        exit_action.triggered.connect(parent.close)
 
         self.setContextMenu(menu)
+
+        self.activated.connect(self.clicked)
+
+    def clicked(self, reason):
+        if reason == QSystemTrayIcon.Trigger:
+            self.parent().unhide()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -173,8 +267,5 @@ if __name__ == "__main__":
     font = QFont("FOT-Pearl Std L", 16)
 
     window = Window()
-
-    tray_icon = TrayIcon(QIcon("icon.png"), window)
-    tray_icon.show()
 
     sys.exit(app.exec_())
